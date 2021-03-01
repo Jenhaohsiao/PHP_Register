3924a2a953ae86e4cec7b18dcc49d005e40ad53a
Jenhao <jenhao.hsiao@gmail.com>
2021-02-28 23:01:10 -0500

    Added basic files and make sure it is able link to DB on my hosting server (The password has removed)

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register home page</title>
+</head>
+
+<body>
+
+    <h2>User Authentication System</h2>
+    <hr>
+
+    <?php  include_once "./resource/Database.php" ?>
+
+
+</body>
+
+</html>
---
+++ resource/Database.php
@@ -0,0 +1,57 @@
+<?php
+
+try {
+    $date = new DateTime();
+} catch (Exception $e) {
+    echo $e->getMessage();
+    exit(1);
+}
+
+echo $date->format('Y-m-d h:m:s');
+?>
+</br>
+<?php
+echo "Try to connnect DB by PDO"; 
+?>
+</br>
+<?php
+$host = 'localhost';
+$database = 'abunda12_phpLearn_Register';
+
+$dsn = 'mysql:host=localhost;dbname=abunda12_phpLearn_Register';
+$user = 'abunda12_phpLearn';
+$passwd = '';
+ 
+try{
+    $db  = new PDO($dsn, $user, $passwd);
+    $db-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connnected to DB successfuly";   
+}catch (PDOException $ex){
+     echo "Connnected to DB failed ".$ex->getMessage();   
+}
+
+?>
+
+
+</br>
+<?php
+
+
+// // another one
+// echo "Try to connnect another DB"; 
+// $host = 'localhost';
+// //改成你登入phpmyadmin帳號
+// $user = 'abunda12_phpLearn';
+// //改成你登入phpmyadmin密碼
+// $passwd = '';
+// //資料庫名稱
+// $database = 'abunda12_phpLearn_Register';
+// //實例化mysqli(資料庫路徑, 登入帳號, 登入密碼, 資料庫)
+// $connect = new mysqli($host, $user, $passwd, $database);
+ 
+// if ($connect->connect_error) {
+//     die("連線失敗: " . $connect->connect_error);
+// }
+// echo "連線成功";
+
+?>
